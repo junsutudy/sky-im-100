abeb72c13cb9e66156a77153f9c74ff268fba467
junsuPark <krpjs0508@gmail.com>
2025-10-24 21:27:24 +0900

    imback app

--- app/src/main/java/app/junsu/imback/MainActivity.kt
+++ app/src/main/java/app/junsu/imback/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import app.junsu.imback.features.main.MainScreen
 import app.junsu.imback.ui.theme.IMBACKTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +18,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IMBACKTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ImBackApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+enum class ImBackDestinations(
+    val route: String,
+) {
+    MAIN(
+        route = "main",
+    ),
+    PHOTO_DETAILS(
+        route = "photo_details",
+    ),
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    IMBACKTheme {
-        Greeting("Android")
+fun ImBackApp(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = ImBackDestinations.MAIN.route,
+    ) {
+        composable(ImBackDestinations.MAIN.route) {
+            MainScreen()
+        }
     }
 }
