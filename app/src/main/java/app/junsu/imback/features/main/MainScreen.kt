39d9cf17fbffad0b0c3184beaeef549d9150a79e
junsuPark <krpjs0508@gmail.com>
2025-10-24 21:42:06 +0900

    navigation

--- app/src/main/java/app/junsu/imback/features/main/MainScreen.kt
+++ app/src/main/java/app/junsu/imback/features/main/MainScreen.kt
@@ -1,7 +1,21 @@
 package app.junsu.imback.features.main
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -10,28 +24,58 @@ import app.junsu.imback.features.main.tabs.photo_list.PhotoListTab
 
 enum class MainDestinations(
     val route: String,
+    val icon: ImageVector,
 ) {
     PHOTO_LIST(
         route = "/photo_list",
+        icon = Icons.AutoMirrored.Filled.List,
     ),
     LIBRARY(
         route = "/library",
-    ),
+        icon = Icons.Default.Email
+    ), ;
 }
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
-    NavHost(
-        navController = navController,
-        startDestination = MainDestinations.PHOTO_LIST.route,
+    var selectedTab by remember { mutableStateOf(MainDestinations.PHOTO_LIST) }
+
+    Scaffold(
         modifier = modifier,
-    ) {
-        composable(MainDestinations.PHOTO_LIST.route) {
-            PhotoListTab()
+        bottomBar = {
+            BottomAppBar {
+                MainDestinations.entries.forEach { destination ->
+                    NavigationBarItem(
+                        selected = destination == selectedTab,
+                        onClick = {
+                            selectedTab = destination
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = destination.icon,
+                                contentDescription = null,
+                            )
+                        },
+                        label = {
+                            Text("Photos")
+                        },
+                    )
+                }
+            }
         }
-        composable(MainDestinations.LIBRARY.route) {
-            LibraryTab()
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = MainDestinations.PHOTO_LIST.route,
+            modifier = Modifier.padding(paddingValues),
+        ) {
+            composable(MainDestinations.PHOTO_LIST.route) {
+                PhotoListTab()
+            }
+            composable(MainDestinations.LIBRARY.route) {
+                LibraryTab()
+            }
         }
     }
 }
