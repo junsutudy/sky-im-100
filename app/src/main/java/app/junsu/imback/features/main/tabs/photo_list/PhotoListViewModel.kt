dd557f9fe575a4a5432a071b8c2b5d59c88a2888
junsuPark <krpjs0508@gmail.com>
2025-10-25 16:37:18 +0900

    fetching

--- app/src/main/java/app/junsu/imback/features/main/tabs/photo_list/PhotoListViewModel.kt
+++ app/src/main/java/app/junsu/imback/features/main/tabs/photo_list/PhotoListViewModel.kt
@@ -4,9 +4,12 @@ import androidx.lifecycle.ViewModel
 import app.junsu.imback.data.photo.PhotoRepository
 import app.junsu.imback.data.photo.model.Photo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,6 +19,16 @@ class PhotoListViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(PhotoListUiState())
     val uiState: StateFlow<PhotoListUiState>
         get() = _uiState.asStateFlow()
+
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val fetchedPhotos = photoRepository.getPhotos(page = 0, limit = 50)
+                _uiState.emit(uiState.value.copy(photos = fetchedPhotos))
+            } catch (_: Exception) {
+            }
+        }
+    }
 }
 
 data class PhotoListUiState(
