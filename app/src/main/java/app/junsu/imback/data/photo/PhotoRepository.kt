3961db6a38c9f29744003135ab40dffedd047b5f
junsuPark <krpjs0508@gmail.com>
2025-10-25 13:54:09 +0900

    respo

--- app/src/main/java/app/junsu/imback/data/photo/PhotoRepository.kt
+++ app/src/main/java/app/junsu/imback/data/photo/PhotoRepository.kt
@@ -1,4 +1,35 @@
 package app.junsu.imback.data.photo
 
-class PhotoRepository {
+import app.junsu.imback.data.photo.model.Photo
+import app.junsu.imback.datasource.photo.PhotoDatabaseDataSource
+import app.junsu.imback.datasource.photo.PhotoNetworkDataSource
+import javax.inject.Inject
+
+class PhotoRepository @Inject constructor(
+    private val photoNetworkDataSource: PhotoNetworkDataSource,
+    private val photoDatabaseDataSource: PhotoDatabaseDataSource,
+) {
+    suspend fun getPhotoList(
+        page: Int = 0,
+        limit: Int = PHOTOS_FETCH_LIMIT,
+    ): List<Photo> {
+        val fetchedPhotos = photoNetworkDataSource.fetchPhotos(
+            page = page,
+            limit = limit,
+        )
+        return fetchedPhotos.map { response ->
+            Photo(
+                id = response.id,
+                author = response.author,
+                width = response.width,
+                height = response.height,
+                url = response.url,
+                downloadUrl = response.downloadUrl,
+            )
+        }
+    }
+
+    companion object {
+        const val PHOTOS_FETCH_LIMIT = 50
+    }
 }
