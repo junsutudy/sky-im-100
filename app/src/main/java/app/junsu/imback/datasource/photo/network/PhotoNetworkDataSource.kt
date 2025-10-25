0f42dbc0a382c7fa9e4ce1ae1a27d46a4dff40cb
junsuPark <krpjs0508@gmail.com>
2025-10-25 15:57:24 +0900

    ds impl

--- app/src/main/java/app/junsu/imback/datasource/photo/network/PhotoNetworkDataSource.kt
+++ app/src/main/java/app/junsu/imback/datasource/photo/network/PhotoNetworkDataSource.kt
@@ -1,10 +1,24 @@
 package app.junsu.imback.datasource.photo.network
 
+import app.junsu.imback.data.photo.PhotoRepositoryImpl.Companion.PHOTOS_FETCH_LIMIT
+import app.junsu.imback.datasource.photo.model.PhotoResponse
 import app.junsu.imback.datasource.photo.network.api_service.PhotoApiService
 import javax.inject.Inject
 
-abstract class PhotoNetworkDataSource
+abstract class PhotoNetworkDataSource {
+    abstract suspend fun fetchPhotos(page: Int?, limit: Int?): List<PhotoResponse>
+}
 
 class PhotoNetworkDataSourceImpl @Inject constructor(
     private val photoApiService: PhotoApiService,
-) : PhotoNetworkDataSource()
+) : PhotoNetworkDataSource() {
+    override suspend fun fetchPhotos(
+        page: Int?,
+        limit: Int?,
+    ): List<PhotoResponse> {
+        return photoApiService.fetchPhotos(
+            page = page ?: 0,
+            limit = limit ?: PHOTOS_FETCH_LIMIT,
+        )
+    }
+}
