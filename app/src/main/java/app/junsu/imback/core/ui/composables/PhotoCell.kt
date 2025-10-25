925846ff4fab528f85ce0bffdf2a5f817fd61815
junsuPark <krpjs0508@gmail.com>
2025-10-25 13:54:14 +0900

    cell moved

---
+++ app/src/main/java/app/junsu/imback/core/ui/composables/PhotoCell.kt
@@ -0,0 +1,14 @@
+package app.junsu.imback.core.ui.composables
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import app.junsu.imback.data.photo.model.Photo
+
+@Composable
+fun PhotoCell(
+    photo: Photo,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+
+}
--- app/src/main/java/app/junsu/imback/core/ui/photo/PhotoCell.kt
+++
@@ -1,9 +0,0 @@
-package app.junsu.imback.core.ui.photo
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun PhotoCell(modifier: Modifier = Modifier) {
-
-}
