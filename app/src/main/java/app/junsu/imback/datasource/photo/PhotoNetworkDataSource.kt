6ae2373f65518b8420aab0b548ee4638a1cc6824
junsuPark <krpjs0508@gmail.com>
2025-10-25 13:55:19 +0900

    new

---
+++ app/src/main/java/app/junsu/imback/datasource/photo/PhotoNetworkDataSource.kt
@@ -0,0 +1,21 @@
+package app.junsu.imback.datasource.photo
+
+import app.junsu.imback.datasource.photo.model.PhotoDetailsResponse
+import app.junsu.imback.datasource.photo.model.PhotoResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PhotoNetworkDataSource {
+
+    @GET("/v2/list")
+    suspend fun fetchPhotos(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int = 20,
+    ): List<PhotoResponse>
+
+    @GET("/id/{photo_id}/info")
+    suspend fun fetchPhotoDetails(
+        @Path("photo_id") photoId: String,
+    ): PhotoDetailsResponse
+}
