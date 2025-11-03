12fb939ce7cc52b57f6690f7d03eadce959b77a7
junsuPark <krpjs0508@gmail.com>
2025-11-03 22:16:35 +0900

    photo scr

---
+++ app/src/main/java/app/junsu/imback/features/photo_details/PhotoDetailsScreen.kt
@@ -0,0 +1,76 @@
+package app.junsu.imback.features.photo_details
+
+import PhotoViewer
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.collectSideEffect
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PhotoDetailsScreen(
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: PhotoDetailsViewModel = hiltViewModel(),
+) {
+    val state by viewModel.collectAsState()
+
+    viewModel.collectSideEffect {
+        when (it) {
+            PhotoDetailsSideEffect.Loading -> println("PHOTO DETAILS LOADING ENTERED")
+        }
+    }
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text("Photo Details") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onNavigateUp,
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = null,
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            println("BOOKMARK")
+                        },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Bookmark,
+                            contentDescription = null,
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier,
+        ) {
+            PhotoViewer(
+                photoDetails = state.photoDetails,
+            )
+        }
+    }
+}
