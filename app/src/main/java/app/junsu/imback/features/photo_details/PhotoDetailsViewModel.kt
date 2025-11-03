048b6c9cbc4005cf5541701bfb475c4917cd7812
junsuPark <krpjs0508@gmail.com>
2025-11-03 22:16:38 +0900

    viewmodel

---
+++ app/src/main/java/app/junsu/imback/features/photo_details/PhotoDetailsViewModel.kt
@@ -0,0 +1,29 @@
+package app.junsu.imback.features.photo_details
+
+import androidx.lifecycle.ViewModel
+import app.junsu.imback.data.photo.PhotoRepository
+import app.junsu.imback.data.photo.model.PhotoDetails
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+data class PhotoDetailsState(
+    val photoDetails: PhotoDetails? = null,
+)
+
+sealed class PhotoDetailsSideEffect {
+    data object Loading : PhotoDetailsSideEffect()
+}
+
+@HiltViewModel
+class PhotoDetailsViewModel @Inject constructor(
+    val photoRepository: PhotoRepository,
+) : ContainerHost<PhotoDetailsState, PhotoDetailsSideEffect>, ViewModel() {
+    override val container =
+        container<PhotoDetailsState, PhotoDetailsSideEffect>(PhotoDetailsState())
+
+    fun abc() {
+
+    }
+}
