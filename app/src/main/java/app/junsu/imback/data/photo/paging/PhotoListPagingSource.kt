1b6820e1f2a177640b5621d64248687f1004c87d
junsuPark <krpjs0508@gmail.com>
2025-11-03 23:08:30 +0900

    pg

--- app/src/main/java/app/junsu/imback/data/photo/paging/PhotoListPagingSource.kt
+++ app/src/main/java/app/junsu/imback/data/photo/paging/PhotoListPagingSource.kt
@@ -3,16 +3,14 @@ package app.junsu.imback.data.photo.paging
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import app.junsu.imback.data.photo.model.Photo
-import app.junsu.imback.data.photo.repository.PhotoRepository
 import kotlinx.coroutines.delay
-import javax.inject.Inject
 import kotlin.math.max
 
 private const val STARTING_KEY = 0
 private const val LOAD_DELAY_MILLIS = 3_000L
 
-class PhotoListPagingSource @Inject constructor(
-    val photoRepository: PhotoRepository,
+class PhotoListPagingSource(
+    val getPhotos: suspend (page: Int, limit: Int) -> List<Photo>,
 ) : PagingSource<Int, Photo>() {
     override fun getRefreshKey(state: PagingState<Int, Photo>): Int? {
         return state.anchorPosition?.let {
@@ -23,23 +21,20 @@ class PhotoListPagingSource @Inject constructor(
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Photo> {
         val startKey = params.key ?: STARTING_KEY
-        val response =
-            photoRepository.getPhotos(page = startKey, limit = params.loadSize)
+        val response = getPhotos(startKey, params.loadSize)
 
         if (startKey != STARTING_KEY) {
             delay(LOAD_DELAY_MILLIS)
         }
 
         return LoadResult.Page(
-            data = response,
-            prevKey = when (startKey) {
+            data = response, prevKey = when (startKey) {
                 STARTING_KEY -> null
                 else -> when (val prevKey = ensureValidKey(key = startKey - params.loadSize)) {
                     STARTING_KEY -> null
                     else -> prevKey
                 }
-            },
-            nextKey = startKey + 1
+            }, nextKey = startKey + 1
         )
     }
 
--- app/src/main/java/app/junsu/imback/features/photo_details/PhotoDetailsViewModel.kt
+++ app/src/main/java/app/junsu/imback/features/photo_details/PhotoDetailsViewModel.kt
@@ -1,8 +1,11 @@
 package app.junsu.imback.features.photo_details
 
 import androidx.lifecycle.ViewModel
-import app.junsu.imback.data.photo.repository.PhotoRepository
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import app.junsu.imback.data.photo.model.PhotoDetails
+import app.junsu.imback.data.photo.paging.PhotoListPagingSource
+import app.junsu.imback.data.photo.repository.PhotoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.viewmodel.container
@@ -22,6 +25,12 @@ class PhotoDetailsViewModel @Inject constructor(
 ) : ContainerHost<PhotoDetailsState, PhotoDetailsSideEffect>, ViewModel() {
     override val container =
         container<PhotoDetailsState, PhotoDetailsSideEffect>(PhotoDetailsState())
+
+    val pager = Pager(PagingConfig(pageSize = 50)) {
+        PhotoListPagingSource(getPhotos = { page, limit ->
+            photoRepository.getPhotos(page = page, limit = limit)
+        })
+    }
 
     fun loadPhotoDetails(photoId: Long) {
         intent {
